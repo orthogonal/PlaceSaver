2141d81fd3a3bd27928db27af86be11e8787ea87
unknown <Will@.(none)>
2011-12-23 16:39:56 -0800

    Place Saver by William B. Wettersten and Andrew Latham.

---
+++ src/MobiTech/PlaceSaver/MapActivity.java
@@ -0,0 +1,15 @@
+package MobiTech.PlaceSaver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
