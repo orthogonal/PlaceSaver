19db2267414195059e8ab6282cc3f745be4f042e
Andrew Latham <acl68@case.edu>
2011-12-31 14:21:58 -0500

    Redid the maps overlay setup.  With comments!  Lots of comments! I found a really good book (I know it must be really good because it's 1200 pages long) that explains the Maps and TouchScreen API a lot better. It's much better than the Google tutorial and documentation.

--- Andrew's Temporary Model/PlaceSaver/gen/MobiTech/PlaceSaver/R.java
+++ Andrew's Temporary Model/PlaceSaver/gen/MobiTech/PlaceSaver/R.java
@@ -19,17 +19,23 @@ public final class R {
         public static final int redflag=0x7f020002;
     }
     public static final class id {
-        public static final int do_nothing=0x7f080009;
+        public static final int do_nothing=0x7f08000f;
         public static final int mainAboutButton=0x7f080005;
         public static final int mainHelpButton=0x7f080004;
         public static final int mainMapButton=0x7f080001;
         public static final int mainNewButton=0x7f080003;
         public static final int mainOldButton=0x7f080002;
-        public static final int mapView=0x7f080006;
-        public static final int nothing_message=0x7f080008;
+        public static final int mapview=0x7f08000c;
+        public static final int newRemindersText=0x7f08000d;
+        public static final int normal=0x7f08000b;
+        public static final int nothing_message=0x7f08000e;
         public static final int oldRemindersText=0x7f080000;
-        public static final int options=0x7f08000a;
-        public static final int zoom=0x7f080007;
+        public static final int options=0x7f080010;
+        public static final int sat=0x7f080008;
+        public static final int street=0x7f080009;
+        public static final int traffic=0x7f08000a;
+        public static final int zoomin=0x7f080006;
+        public static final int zoomout=0x7f080007;
     }
     public static final class layout {
         public static final int about=0x7f030000;
--- Andrew's Temporary Model/PlaceSaver/src/MobiTech/PlaceSaver/Maps1.java
+++ Andrew's Temporary Model/PlaceSaver/src/MobiTech/PlaceSaver/Maps1.java
@@ -1,49 +1,144 @@
 package MobiTech.PlaceSaver;
 
-import java.util.List;
 
-import android.app.Activity;
-import android.graphics.drawable.Drawable;
+import java.util.ArrayList;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
-import com.google.android.maps.MapView.LayoutParams;  
-import android.view.View;
-import android.widget.LinearLayout;
 
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class Maps1 extends MapActivity { 
+public class Maps1 extends MapActivity{ 
 
+	private MapView mapView;
+	/**A MapView is one interactive map.  So we declare this as an object of the entire class since it's going to be used
+	 * in a lot of different methods in this MapActivity.
+	 */
+	
+	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.maps);
+		setContentView(R.layout.maps); 
 		
-		MapView mapView = (MapView) findViewById(R.id.mapView);
-        mapView.setBuiltInZoomControls(true);
-        
-        
-        List<Overlay> flags = mapView.getOverlays();
-        Drawable drawable = this.getResources().getDrawable(R.drawable.redflag); 
-        FlaggedLocation flagloc = new FlaggedLocation(drawable, this);
+		mapView = (MapView)findViewById(R.id.mapview);
+		/**This line sets the handler for the MapView for the entire class, mapView equal to the displayed on the screen from the XML file.  **/
        
-        
-        GeoPoint point = new GeoPoint(38914887, -94640801);
-        OverlayItem overlayitem = new OverlayItem(point, "Where I am", "Starbucks in Barnes and Noble");
-        //This defines a new overlay item at the point specified there.  
-        //Note that points are specified in degrees x 10^6.
-        //The two strings are the title and the snippet text for that item.
-        
-        flagloc.add(overlayitem);
-        flags.add(flagloc);
-        //Adds it to the collection.
+		Drawable marker = getResources().getDrawable(R.drawable.redflag);
+		marker.setBounds((int)(-marker.getIntrinsicWidth()/2), -marker.getIntrinsicHeight(), (int)(marker.getIntrinsicWidth()/2), 0);
+		/**These two lines set up the marker for use with an ItemizedOverlay.  
+		 * The first one sets the Drawable object marker equal to the redflag in the res/drawable folder.
+		 * The next one sets the (left, top, right, bottom) coordinates.  This is so that the Maps API knows where the (0, 0) point is.
+		 * In this case, it's halfway across the bottom edge.  So the bottom-left corner is (half, 0) or the bottom point of the flagpole.
+		 * super(boundCenterBottom(marker)); does the same thing.
+		 */
+		
+		
+		InterestingLocations funPlaces = new InterestingLocations(marker);
+		mapView.getOverlays().add(funPlaces);
+		/**This adds the overlay defined in the private class below.
+		 * First, it instantiates the overlay.  This will call the constructor, which adds the two points in it.
+		 * Then we add that overlay to the mapView so that it shows up.
+		 */
+		
+		GeoPoint pt = new GeoPoint(38914887, -94640821);
+		int latSpan = funPlaces.getLatSpanE6();
+		int lonSpan = funPlaces.getLonSpanE6();
+		Log.v("Overlays", "Lat span is " + latSpan);
+		Log.v("Overlays", "Lon span is " + lonSpan);
+		
+		MapController mc = mapView.getController();
+		mc.setCenter(pt);
+		mc.zoomToSpan((int)(latSpan*1.5), (int)(lonSpan*1.5));
+		
 	}
 	
+	/**The below section makes extensive use of mapView.getController(), which is used to zoom. **/
+	/**It is also easy to change the view - there are just boolean methods for each view.  **/
+	public void myClickHandler(View target) {
+		switch(target.getId()) {
+		case R.id.zoomin:
+			mapView.getController().zoomIn();
+			break;
+		case R.id.zoomout:
+			mapView.getController().zoomOut();
+			break;
+		case R.id.sat:
+			mapView.setSatellite(true);
+			break;
+		case R.id.street:
+			mapView.setStreetView(true);
+			break;
+		case R.id.traffic:
+			mapView.setTraffic(true);
+			break;
+		case R.id.normal:
+			mapView.setSatellite(false);
+			mapView.setStreetView(false);
+			mapView.setTraffic(false);
+			break;
+		}
+		mapView.postInvalidateDelayed(2000);  //This is just one of those mysterious things that HAS to be there.
+	}
+	/**Addition of the below two methods is required by the Google Maps TOS.  That's the only reason they're here.  **/
+	@Override
+	protected boolean isLocationDisplayed() {
+		return false;
+	}	
 	@Override
 	protected boolean isRouteDisplayed(){
 		return false;
 	}
+	
+	class InterestingLocations extends ItemizedOverlay {
+		/**The Overlay class itself defines the contract for an overlay, so you can't instantiate it.
+		*ItemizedOverlay is an implementation that is oriented for the creation of new items in the manner we want.
+		**/
+		private ArrayList<OverlayItem> locations = new ArrayList<OverlayItem>();
+		/**locations is a collection of points.**/
+		
+		public InterestingLocations(Drawable marker)
+		{
+			/**This is the constructor for this class.  It creates a couple points and then calls populate() to add them.  **/
+			
+			super(marker);
+			
+			GeoPoint point = new GeoPoint(38914887, -94640801);
+			GeoPoint point2 = new GeoPoint(38914900, -94640821);
+			
+			locations.add(new OverlayItem(point, "Point 1", "Point 1"));
+			locations.add(new OverlayItem(point2, "Point 2", "Point 2"));
+			
+			populate();
+			/**populate() caches any OverlayItems.  It first calls the size() method to determine the number of items,
+			*and then goes into a loop using that information to call createItem(i) for each item.  
+			**/
+		}
+		
+		@Override
+		public void draw(Canvas canvas, MapView mapView, boolean shadow)
+		{
+			super.draw(canvas, mapView, shadow);
+		}
+		
+		@Override
+		protected OverlayItem createItem(int i) {
+			return locations.get(i);
+		}
+		
+		@Override
+		public int size() {
+			return locations.size();
+		}
+		
+	}
+	
+	
 }
