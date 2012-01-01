8d707fcd8d04184026945ec3beae26a796c7c71a
Andrew Latham <acl68@case.edu>
2012-01-01 16:48:51 -0600

    Every activity that needs to be made is made, has an empty layout, and is set up properly in the manifest file.

--- Final Copy/PlaceSaver/gen/MobiTech/PlaceSaver/R.java
+++ Final Copy/PlaceSaver/gen/MobiTech/PlaceSaver/R.java
@@ -25,6 +25,8 @@ public final class R {
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int manager=0x7f030001;
+        public static final int newreminder=0x7f030002;
+        public static final int oldreminder=0x7f030003;
     }
     public static final class menu {
         public static final int mapmenu=0x7f050000;
---
+++ Final Copy/PlaceSaver/src/MobiTech/PlaceSaver/NewReminder.java
@@ -0,0 +1,14 @@
+package MobiTech.PlaceSaver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewReminder extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.newreminder);
+	}
+
+}
